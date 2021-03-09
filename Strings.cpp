87d91d285ed6042331249520dfab96911c115563
Hritika Sanjay Dhasal <66423487+HellFire03@users.noreply.github.com>
2021-03-09 22:58:08 +0530

    Add files via upload

---
+++ Strings.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int len(string var){
+    return var.size();
+}
+
+string add(string a, string b){
+    return a+b;
+}
+
+void swap(string &a, string &b){
+    int temp;
+    temp=a[0];
+    a[0]=b[0];
+    b[0]=temp;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+    string a,b,temp;
+    cin>>a >> b;
+    cout << len(a)<<" "<<len(b)<<"\n"<<add(a,b)<<"\n";
+    swap(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
